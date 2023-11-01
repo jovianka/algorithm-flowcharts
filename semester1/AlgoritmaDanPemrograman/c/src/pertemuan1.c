a1ee2f8da6f20656135f02cac7a486249d6747e6
jovianka <jovianpratama123@gmail.com>
2023-11-01 09:16:47 +0800

    updated alpro

---
+++ semester1/AlgoritmaDanPemrograman/c/src/pertemuan1.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+
+int main()
+{
+  // LUAS SEGIEMPAT
+  // int p, l, luas;
+
+  // printf("Program Luas Sg4\n");
+  // printf("============================\n");
+  // printf("Ketik Panjang: ");
+  // scanf("%d", &p);
+  // printf("Ketik Lebar: ");
+  // scanf("%d", &l);
+  
+  // luas = p * l;
+  // printf("Luas Sg4 dengan p = %d dan l = %d adalah %d\n", p, l, luas);
+
+
+  // GANJIL GENAP
+  // int bil;
+
+  // printf("Masukkan bilangan");
+  // scanf("%d", &bil);
+
+  // if (bil % 2 == 0)
+  // {
+  //   printf("GENAP!");
+  // }
+  // else
+  // {
+  //   printf("GANJIL!");
+  // }
+
+
+  // PIRAMIDA KEBALIK
+  int baris;
+  printf("Berapa baris? ");
+  scanf("%d", &baris);
+
+  for (int i = 0; i < baris; i++)
+  {
+    for (int j = 0; j < i; j++)
+    {
+      printf("  ");
+    }
+
+    for (int k = 1; k < (baris - i) * 2; k++)
+    {
+      printf("* ");
+    }
+
+    printf("\n");
+  }
+
+
+  return 0;
+}
